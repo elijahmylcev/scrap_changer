46d23284a3ebabfe7386694ed6ecbf574b380d3b
Elijah Mylcev <elijahmylcev@gmail.com>
2023-01-10 12:35:14 +0600

    array in .env

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import requests
+import json
 import os
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+load_dotenv()
 BASE_URL = os.getenv('BASE_URL')
-ROUNDS = os.getenv('ROUNDS')
+data_arr = os.getenv('ROUNDS')
+ROUNDS = json.loads(data_arr)
 
 print(ROUNDS)
 
-# res = requests.get(URL)
+res = requests.get(BASE_URL+ROUNDS[0])
+
+print(res)
