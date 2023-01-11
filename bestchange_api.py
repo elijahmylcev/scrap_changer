69a8b23929d339445c77ef98d11a8f446271d43f
Elijah Mylcev <elijahmylcev@gmail.com>
2023-01-11 15:29:52 +0600

    fix

--- bestchange_api.py
+++ bestchange_api.py
@@ -16,23 +16,32 @@ def getData(URL):
   z = zipfile.ZipFile(io.BytesIO(result.content))
   z.extractall("./currency")
   
-def create_info_table(path):
+def create_info_df(path):
   columns = {0: 'id_currency', 2: 'name_currency', 3:'short_name'}
 
   df = pd.read_csv(path, delimiter=';', encoding='windows-1251', header=None)
   df = df[df.columns[[0, 2, 3]]]
   df = df.rename(columns=columns)
   df.to_csv('csv.csv')
+  return df
 
 
-def create_df(path):
+def create_rates_df(path):
   columns=['ID отдаваемой валюты', 'ID получаемой валюты', 'ID обменного пункта', 'Курс обмена (отдать)', 'Курс обмена (получить)', 'Резерв получаемой валюты', 'Отзывы', 'Не используется', 'Минимальная сумма обмена', 'Максимальная сумма обмена', 'ID города (для направлений с наличными)']
   df = pd.read_csv(path, delimiter=';', encoding='cp1251', names=columns)
+  df.to_csv('currency.csv')
+  return df
+
+def first_calc(rates_df, id_currency_star, id_currency_end=None):
+ search_df = rates_df[(rates_df['ID отдаваемой валюты'] == id_currency_star) & (rates_df['ID получаемой валюты'] == id_currency_end)]
+ search_df = min(search_df['Курс обмена (отдать)'])
+ print(search_df)
 
 
 if __name__ == '__main__':
-  # getData(URL_API)
-  # create_df('./currency/bm_rates.dat')
-  create_info_table('./currency/bm_cy.dat')
+  getData(URL_API)
+  rates_df = create_rates_df('./currency/bm_rates.dat')
+  info = create_info_df('./currency/bm_cy.dat')
+  first_calc(rates_df, 105, 208)
   
   
